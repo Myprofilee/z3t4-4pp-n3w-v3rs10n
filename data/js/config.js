046624114263c401aef236af40c6ddab6ca7c55a
Myprofilee <andrep13.id@gmail.com>
2022-12-20 19:04:26 +0700

    first commit

---
+++ data/js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDCTTnUof2ABs1WrLQH6yokX0Xon-XhNtU",
+    authDomain: "zetachat.firebaseapp.com",
+    databaseURL: "https://zetachat-default-rtdb.firebaseio.com",
+    projectId: "zetachat",
+    storageBucket: "zetachat.appspot.com",
+    messagingSenderId: "573300156255",
+    appId: "1:573300156255:web:427a1b9335610b5af878ea"
+};
